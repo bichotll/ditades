50a54c61efaa8f66876570b7517d6b497880dac2
bichotll <jaume@bic.cat>
2012-12-03 23:35:37 +0100

    primeres estructures backbones

--- game/src/js/game.js
+++ game/src/js/game.js
@@ -1,7 +1,7 @@
 /*vistas*/
 window.HomeView = Backbone.View.extend({
 
-    template:_.template($('#home').html()),
+    template:_.template($('#tmp_game').html()),
 
     render:function (eventName) {
         $(this.el).html(this.template());
@@ -9,55 +9,63 @@ window.HomeView = Backbone.View.extend({
     }
 });
 
-window.Page1View = Backbone.View.extend({
-
-    template:_.template($('#page1').html()),
-
-    render:function (eventName) {
-        $(this.el).html(this.template());
-        return this;
-    }
-});
-
-window.Page2View = Backbone.View.extend({
-
-    template:_.template($('#page2').html()),
-
-    render:function (eventName) {
-        $(this.el).html(this.template());
-        return this;
-    }
-});
 
 /*model*/
 Game = Backbone.Model.extend({
     initialize: function(){
+        console.log('new game');
         this.game = true;
+        ply = {};
+        ply[0] = new Player();
+        ply[1] = new Player();
         this.init_tablero();
-        ply1 = new Player;
     },
     defaults: {
+        ply: '',
         height: '',
         width: '',
         game: false,
+        width_click: '',
     },
     init_tablero: function(){
         this.height = window.innerHeight;
         this.width = window.innerWidth;
+        this.width_click = this.width/25;
+        ply[0].set({
+                element:'p_ply0',
+                height:this.height,
+                width:(this.width/2)
+        });
+        ply[1].set({
+                element:'p_ply1',
+                height:this.height,
+                width:(this.width/2)
+        });
+        ply[0].set_dimensions();
+        ply[1].set_dimensions();
+        console.log('init tablero');
     }
 });
+
 Player = Backbone.Model.extend({
     initialize: function(){
-        this.color = '#'+Math.floor(Math.random()*16777215).toString(16);
+        this.set({'color' : '#'+Math.floor(Math.random()*16777215).toString(16)});
     },
     defaults: {
         nom: 'Default title',
         color: '',
+        height: '',
+        width: '',
+        element: '',
+    },
+    set_dimensions: function(){
+        console.log(this.get('color'));
+        $('#'+this.get('element')).css('background', this.get('color'));
+        $('#'+this.get('element')).css('height', this.get('height')+'px');
+        $('#'+this.get('element')).css('width', this.get('width')+'px');
     }
-})
+});
 
-
-$(document).ready(function () {
-    console.log('document ready');
-    game = new game();
+Zepto(function($){
+    game = new Game();
 });
